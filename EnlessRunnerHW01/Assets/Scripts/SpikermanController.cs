1efb7c0667512c42ae233d171743a3781cd10433
AugmentOfRebirth <theaugmentofrebirth@gmail.com>
2025-06-23 23:46:33 -0400

    oops forgot about this

--- EnlessRunnerHW01/Assets/Scripts/SpikermanController.cs
+++ EnlessRunnerHW01/Assets/Scripts/SpikermanController.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 public class SpikermanController : MonoBehaviour
 {
     public float moveSpeed;
-    private Rigidbody2D rb;
+    private Rigidbody2D spikermanRb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        spikermanRb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(-moveSpeed, rb.linearVelocity.y);
+        spikermanRb.linearVelocity = new Vector2(-moveSpeed, spikermanRb.linearVelocity.y);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
